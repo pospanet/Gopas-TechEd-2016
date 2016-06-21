02eaab4cdc8bab3ccf2970957916cc778e544067
pospa <pospa@pospa.net>
2016-06-21 09:30:11 +0200

    On-site changes

--- Sense HAT/MainPage.xaml.cs
+++ Sense HAT/MainPage.xaml.cs
@@ -14,7 +14,7 @@ namespace Sense_HAT
             InitializeComponent();
             //RPi.SenseHat.Demo.DemoRunner.Run(senseHat => new RPi.SenseHat.Demo.Demos.DiscoLights(senseHat, this));
             //RPi.SenseHat.Demo.DemoRunner.Run(senseHat => new RPi.SenseHat.Demo.Demos.JoystickPixel(senseHat, this));
-            //RPi.SenseHat.Demo.DemoRunner.Run(senseHat => new RPi.SenseHat.Demo.Demos.SingleColorScrollText(senseHat, this, "Gopas TechEd"));
+            RPi.SenseHat.Demo.DemoRunner.Run(senseHat => new RPi.SenseHat.Demo.Demos.SingleColorScrollText(senseHat, this, "Gopas TechEd"));
             //RPi.SenseHat.Demo.DemoRunner.Run(senseHat => new RPi.SenseHat.Demo.Demos.GammaTest(senseHat, this));
         }
 
